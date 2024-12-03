7c77daf43f375bad7256b0563f6477a81fbdfa97
PedroUFirmani <106184532+PedroUFirmani@users.noreply.github.com>
2024-12-03 09:54:59 -0300

    Aula 2 pedro

---
+++ Aula 2/02Pedro.py
@@ -0,0 +1,13 @@
+print ('bom dia a todes')
+
+print('esse é o tal jogo melhor que god of war 4, de usuario para ter uma ideia de como fazer um orçamento para limpar o nome da empresa para que eu possa fazer um novo anúncio')
+
+nome_usuario = 'Pedro Firmani'# tipo -> str = string
+idade_usuario = 20 # tipo -> int = integer
+altura_usuario = 1.77 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print('seja bem-vindo', nome_usuario) 
+print('sua idade é', idade_usuario, 'anos') 
+print('sua altura é', altura_usuario) 
+print('você é maior de idade', maior_idade)
